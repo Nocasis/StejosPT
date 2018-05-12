6d131bd8064def4227b31773a6fb6fc4c2f50e0b
Nocasis <nocasisn@gmail.com>
2018-05-12 15:45:11 +0700

    db_manager updated

--- db_manager.py
+++ db_manager.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-import consts
+from consts import *
 
 def get_db():
     return sqlite3.connect(DB_NAME)
@@ -13,22 +13,28 @@ def prepare_db():
     db.commit()
     with open('controls.json') as f: 
         for record in json.load(f):
-            id_, descr, title, requirement, transport  = tuple(record)
-            cursor.execute('''INSERT OR IGNORE INTO control
+            id_, descr, title, requirement, transport  = tuple(record) # Тут надо update?
+            data = cursor.execute("SELECT EXISTS(SELECT * FROM scandata WHERE id = {})".format(id_))
+            if (data.fetchone()[0] == True):
+                cursor.execute('''UPDATE control SET id = ?, description = ?, title = ?, requirement = ?, transport = ?''',(int(id_), str(descr), str(title), str(requirement), str(transport)))
+            else:
+                cursor.execute('''INSERT INTO control
                 (id, description, title, requirement, transport) 
-         VALUES ({}, \"{}\", \"{}\", \"{}\", \"{}\")'''.format(id_, str(descr), str(title), str(requirement), str(transport)))
+         VALUES (?, \"?\", \"?\", \"?\", \"?\")''',(int(id_), str(descr), str(title), str(requirement), str(transport)))
     db.commit()
     db.close()
 
 def add_control(id_, status):
     db = get_db()
     cursor = db.cursor()
-    complaint_data = cursor.execute("SELECT * FROM control WHERE id={}".format(id_)).fetchone()
+    complaint_data = cursor.execute("SELECT * FROM control WHERE id=?",int(id_)).fetchone()
     id_, descr, title, requirement, transport = complaint_data
-    data = cursor.execute("SELECT EXISTS(SELECT * FROM scandata WHERE id = {})".format(id_))
+    data = cursor.execute("SELECT EXISTS(SELECT * FROM scandata WHERE id = ?)",int(id_))
     if (data.fetchone()[0] == True):
-        cursor.execute("UPDATE scandata SET status = \"{}\"".format(str([STATUSES[status]])))
+        cursor.execute("UPDATE scandata SET status = \"?\"",str([STATUSES[status]]))
     else:
-        cursor.execute("INSERT OR IGNORE INTO scandata(id, status) VALUES({},\"{}\")".format(id_, str([STATUSES[status]]) ))
+        cursor.execute("INSERT OR IGNORE INTO scandata(id, status) VALUES(?,\"?\")",int(id_), str([STATUSES[status]]))
     db.commit()
     db.close()
+    
+prepare_db()
