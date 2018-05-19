a17e5e738075cb9e6ad7edec6533cea45f665e5f
Nocasis <nocasisn@gmail.com>
2018-05-19 12:55:10 +0700

    main

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ if __name__ == '__main__':
     except:
         exit("ERROR: prepare_db")
     for script in [name for name in os.listdir('./scripts') if re.match(".+\.py",name)]:
-        result = importlib.import_module("scripts." + script.split('.')[0]).main()
-        add_control(int(script[:3]), result)
+        try:
+            result = importlib.import_module("scripts." + script.split('.')[0]).main()
+            add_control(int(script[:3]), result)
+        except:
+            print("Script {} is down.".format(script.split('.')[0]))
+            pass
     time_passed = int(time() - start_time)
     scan_time = "{}h {}m {}s".format(time_passed//3600, time_passed%3600//60, time_passed%60)
     make_report(scan_time)
